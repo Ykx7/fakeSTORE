e994f219714e6e37e37de2e26a12cf32ec919747
Ykxx999 <143841023+Ykxx999@users.noreply.github.com>
2025-05-24 20:33:13 +0100

    '

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-fetch('https://fakestoreapi.com/products/').then(res=>res.json()).then((data)=>console.log(data));
+fetch('https://fakestoreapi.com/products/')
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(error => console.log('ERROR'));
+
+
+
+
 let products = document.getElementById("")
