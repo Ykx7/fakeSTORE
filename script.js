92c50ba61c833b8c4828ac85761633c857bd0ccc
Ykxx999 <143841023+Ykxx999@users.noreply.github.com>
2025-05-25 12:43:56 +0100

    new things

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-fetch('https://fakestoreapi.com/products/')
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'));
+let url = 'https://fakestoreapi.com/products/';
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let container = document.querySelector(".container");
 
-
-
-
-let products = document.getElementById("")
+        data.forEach(product => {
+            let card = document.createElement("div");
+            card.className = "card";
+            card.innerHTML = `
+                <img src="${product.image}" alt="${product.title}">
+                <h1>${product.title}</h1>
+                <span>Price: $${product.price}</span>
+                <span>Category: ${product.category}</span>
+            `;
+            container.appendChild(card);
+        });
+    })
+    .catch(error => console.log('ERROR:', error));
